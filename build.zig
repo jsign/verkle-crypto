3b4d8eb8be57e0ae5ab6dac849c9d6d1610a3b9e
Ignacio Hagopian <jsign.uy@gmail.com>
2023-06-07 18:40:36 -0300

    build: fix build script

--- build.zig
+++ build.zig
@@ -12,11 +12,15 @@ pub fn build(b: *std.Build) void {
     });
     lib.install();
 
+    // _ = b.createModule(.{
+    //     .source_file = .{ .path = "ecc/bandersnatch/bandersnatch.zig" },
+    // });
+
     const main_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&main_tests.run().step);
 }
