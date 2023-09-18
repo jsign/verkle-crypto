ad37df8c187ec6c5b22492508addef24b0057933
Ignacio Hagopian <jsign.uy@gmail.com>
2023-09-18 14:17:39 -0300

    bench: add inv and sqrt
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- src/bench.zig
+++ src/bench.zig
@@ -3,12 +3,38 @@ const fieldapi = @import("ecc/bandersnatch/fieldapi.zig");
 const Fp = fieldapi.Fp;
 
 pub fn main() void {
-    const a = Fp.fromInteger(12345);
+    const N = 10_000;
+    const fps: [N]Fp = get_fps(N);
+    var start: i64 = undefined;
 
-    const n = 1_000;
-    var start = std.time.microTimestamp();
-    for (0..n) |_| {
-        _ = Fp.legendre(a);
+    start = std.time.microTimestamp();
+    for (0..N) |i| {
+        _ = Fp.legendre(fps[i]);
     }
-    std.debug.print("Legendre symbol: {}µs\n", .{@divTrunc((std.time.microTimestamp() - start), (n))});
+    std.debug.print("Legendre symbol: {}µs\n", .{@divTrunc((std.time.microTimestamp() - start), (N))});
+
+    start = std.time.microTimestamp();
+    for (0..N) |i| {
+        _ = Fp.inv(fps[i]).?;
+    }
+    std.debug.print("Inverse: {}µs\n", .{@divTrunc((std.time.microTimestamp() - start), (N))});
+
+    start = std.time.microTimestamp();
+    for (0..N) |i| {
+        _ = Fp.inv(fps[i]).?;
+    }
+    std.debug.print("Sqrt: {}µs\n", .{@divTrunc((std.time.microTimestamp() - start), (N))});
+}
+
+fn get_fps(comptime N: usize) [N]Fp {
+    var fps: [N]Fp = undefined;
+    var i: usize = 0;
+    while (i < N) : (i += 1) {
+        const fe = Fp.fromInteger(i);
+        if (Fp.sqrt(fe) == null) {
+            continue;
+        }
+        fps[i] = Fp.fromInteger(i);
+    }
+    return fps;
 }
